6d456f969f3eafce2e58089b7446ece6a6489296
IanWang <ianwang3240@gmail.com>
2021-10-11 16:18:12 +0800

    Modify train_log plot.

--- ExaTrkXPlots/train_logs.py
+++ ExaTrkXPlots/train_logs.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from matplotlib.ticker import MaxNLocator
 
 from ExaTrkXPlotting import plotter
 
 
 @plotter.plot('exatrkx.train_log', ['history'])
-def train_log(ax, data, tag=None, steps_per_epoch=1, label=None):
+def train_log(ax, data, tag=None, y_label=None, steps_per_epoch=1, label=None):
     history = data['history']
     if tag is None and isinstance(history, list):
         values = history
@@ -16,6 +17,12 @@ def train_log(ax, data, tag=None, steps_per_epoch=1, label=None):
 
     train_epochs = np.arange(0, len(values), 1.0/steps_per_epoch)
     ax.plot(train_epochs, values, label=label)
+
+    ax.set_xlabel('Epochs')
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    
+    if y_label is not None:
+        ax.set_ylabel(y_label)
 
     if label is not None:
         ax.legend()
